08d201dee3b1888f11307b80331575768c7da32b
KiranMR96 <60286317+KiranMR96@users.noreply.github.com>
2020-03-15 19:33:29 +0530

    Add project files.

---
+++ WeatherApp/Model/OneDayForeCast.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Model
+{
+    public class Headline
+    {
+        public DateTime EffectiveDate { get; set; }
+        public int EffectiveEpochDate { get; set; }
+        public int Severity { get; set; }
+        public string Text { get; set; }
+        public string Category { get; set; }
+        public object EndDate { get; set; }
+        public object EndEpochDate { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class MinMax
+    {
+        public int Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+
+    public class TemperatureMinMax
+    {
+        public MinMax Minimum { get; set; }
+        public MinMax Maximum { get; set; }
+    }
+
+    public class DayNight
+    {
+        public int Icon { get; set; }
+        public string IconPhrase { get; set; }
+        public bool HasPrecipitation { get; set; }
+    }
+
+    
+
+    public class DailyForecast
+    {
+        public DateTime Date { get; set; }
+        public int EpochDate { get; set; }
+        public Temperature Temperature { get; set; }
+        public DayNight Day { get; set; }
+        public DayNight Night { get; set; }
+        public IList<string> Sources { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class OnedayForeCast
+    {
+        public Headline Headline { get; set; }
+        public List<DailyForecast> DailyForecasts { get; set; }
+    }
+
+
+}
---
+++ WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+   public class AccuWeatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+        public const string ONEDAYFORECAST_ENDPOINT = "forecasts/v1/daily/1day/{0}?apikey={1}";
+
+        public const string API_KEY = "TpAF6pEhqPYLGeSgZQ4g1OAe8sdHfAuW";
+        
+        public static async Task<List<City>> GetCities(string query) 
+        {
+            List<City> cities = new List<City>();
+
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT,API_KEY,query);
+
+            using(HttpClient client=new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentConditions> GetCurrentConditions(string cityKey)
+        {
+            CurrentConditions currentConditions = new CurrentConditions();
+            string url = BASE_URL + string.Format(CURRENT_CONDITIONS_ENDPOINT, cityKey, API_KEY);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                currentConditions = (JsonConvert.DeserializeObject<List<CurrentConditions>>(json)).FirstOrDefault();
+            }
+            return currentConditions;
+        }
+
+        public static async Task<OnedayForeCast> GetOnedayForeCast( string cityKey)
+        {
+            OnedayForeCast onedayForeCast = new OnedayForeCast();
+            string url = BASE_URL + string.Format(ONEDAYFORECAST_ENDPOINT, cityKey, API_KEY);
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                onedayForeCast = (JsonConvert.DeserializeObject<List<OnedayForeCast>>(json)).FirstOrDefault();
+            }
+            return onedayForeCast;
+        }
+    }
+}
